fd487afffe0f72def36b0f913ca1d35241518b07
Gfgo <gfgo033@hotmail.com>
2022-05-23 13:08:34 -0500

    Update bolsagolpeo.ino

--- bolsagolpeo.ino
+++ bolsagolpeo.ino
@@ -1,87 +1,3 @@
-/*#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-#define PIN       13
-#define PIN2      12
-#define NUMPIXELS 8
-
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixels2(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 500
-
-byte rojo=0;
-byte verde=0;
-byte azul=0;
-const byte PAUSA=1;
-
-byte nump=0;
-byte nump2=0;
-
-int golpe=0;                                              //Lectura de golpe
-int golpe2=0;
-float furk=0;                                             //Fuerza en Kilogramos byte porque es menor a 255
-float furk2=0;
-
-void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  Serial.begin (115200);
-  pixels.begin();
-  pixels.show();
-  pixels2.begin();
-  pixels2.show();
-}
-
-void loop() {
-   pixels.setBrightness(10);
-   pixels2.setBrightness(10);
-//-------------------------------------------------------------lectura de golpe 
-  golpe=analogRead(A0);                                    //Leo analogo 0
-  golpe2=analogRead(A1);                                   //Leo analogo 1
-  
-  nump=map(golpe, 0, 1023, 0, 8);                  //nump es para el nivel fuerza visualizado en barra 61FCRcirc 1023FCRcuadra
-  furk=map(golpe, 0, 61, 0, 9.98);
-  nump2=map(golpe2, 0, 1023, 0, 8);
-  furk2=map(golpe2, 0, 61, 0, 9.98);
-
-  char fuerza[3];
-  sprintf(fuerza, "%d %d", golpe, golpe2);
-  Serial.println(fuerza);
-  //Serial.print('\t');
-  Serial.print(furk); Serial.print('\t'); Serial.print(furk2);
-  Serial.println("Kg");
-//-------------------------------------------------------------luces  
-  pixels.clear();
-  pixels2.clear();
-  for(int i=0; i<nump; i++) {
-    if (i<=1) rojo=0;
-    if ((i>=2)&&(i<=4)) rojo=128;
-    if (i>=4) rojo=255;
-    if (i<=4) verde=255;
-    if (i==5) verde=128;
-    if (i>=6) verde=0;
-    if (i<=7) azul=0;
-    pixels.setPixelColor(i, rojo, verde,  azul);//rojo,verde,azul 0-255
-    pixels.show();
-    delay(PAUSA);
-  }
-
-for(int i2=0; i2<nump2; i2++) {
-    if (i2<=1) rojo=0;
-    if ((i2>=2)&&(i2<=4)) rojo=128;
-    if (i2>=4) rojo=255;
-    if (i2<=4) verde=255;
-    if (i2==5) verde=128;
-    if (i2>=6) verde=0;
-    if (i2<=7) azul=0;
-    pixels2.setPixelColor(i2, rojo, verde,  azul);//rojo,verde,azul 0-255
-    pixels2.show();
-    delay(PAUSA);
-  }
-}*/
-//---------------------
 
 //**********ACELERACION
 //------------------------------------------------------
@@ -262,18 +178,28 @@ const int pin1 = 39;
 const int pin2 = 34;
 const int pin3 = A3;
 const int pin4 = A4;
+const int pin5 = 13;
 
 int golpe0 = 0;  
 int golpe1 = 0;
 int golpe2 = 0;
+int golpe3 = 0;  
+int golpe4 = 0;
+int golpe5 = 0;
 
 byte        nump0=0;
 byte        nump1=0;
 byte        nump2=0;
+byte        nump3=0;
+byte        nump4=0;
+byte        nump5=0;
 
 float       furk0=0;                                             //Fuerza en Kilogramos byte porque es menor a 255
 float       furk1=0;
 float       furk2=0;
+float       furk3=0;                                             
+float       furk4=0;
+float       furk5=0;
 
 void setup() {
   #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -293,11 +219,15 @@ void loop() {
 //furk fuerza en kilogramos de 0-(61 o 1023) y de 0 al limite superior
 //del sensor, para el cuadrado es 20N casi 2 Kg
 
-  golpe0=  analogRead(pin);
-  golpe1 = analogRead(pin1);
-  golpe2 = analogRead(pin2);
+  golpe0= analogRead(pin);
+  golpe1= analogRead(pin1);
+  golpe2= analogRead(pin2);
+  golpe3= analogRead(pin3);
+  golpe4= analogRead(pin4);
+  golpe5= analogRead(pin5);
 
-  Serial.print(golpe0); Serial.print('\t'); Serial.print(golpe1); Serial.print('\t'); Serial.println(golpe2);
+  Serial.print(golpe0); Serial.print('\t'); Serial.print(golpe1); Serial.print('\t'); Serial.print(golpe2); Serial.print('\t');
+  Serial.print(golpe3); Serial.print('\t'); Serial.print(golpe4); Serial.print('\t'); Serial.println(golpe5);
   delay(10);
 
   if(golpe0>=600){
