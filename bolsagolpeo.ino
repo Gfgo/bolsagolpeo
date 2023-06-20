b7e8d54881a9b294a02fea0b2fa3c54cd07ebee9
Gfgo <gfgo033@hotmail.com>
2023-06-20 14:53:00 -0500

    acceleración

--- bolsagolpeo.ino
+++ bolsagolpeo.ino
@@ -1,313 +1,189 @@
+//#include <Adafruit_NeoPixel.h> //esp4095 19/05
+//#ifdef __AVR__
+//  #include <avr/power.h>
+//#endif
+//#define NUMPIXELS   8
+//#define led0        25  //GPIO25
+//#define led1        26  //GPIO26 10  //IO10
+//#define led2        27  //GPIO27
+//Adafruit_NeoPixel strip(NUMPIXELS, led0, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip1(NUMPIXELS, led1, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip2(NUMPIXELS, led2, NEO_GRB + NEO_KHZ800);
+//byte        rojo=0;
+//byte        verde=0;
+//byte        azul=0;
+//const byte  PAUSA=500;
+//
+//const int pin = A0;
+//const int pin1 = 39;
+//const int pin2 = 34;
+//const int pin3 = A3;
+//const int pin4 = A4;
+//const int pin5 = 13;//GPIO13
+//
+//int golpe0 = 0;  
+//int golpe1 = 0;
+//int golpe2 = 0;
+//int golpe3 = 0;  
+//int golpe4 = 0;
+//int golpe5 = 0;
+//
+//byte        nump0=0;
+//byte        nump1=0;
+//byte        nump2=0;
+//byte        nump3=0;
+//byte        nump4=0;
+//byte        nump5=0;
+//
+//float       furk0=0;                                             //Fuerza en Kilogramos byte porque es menor a 255
+//float       furk1=0;
+//float       furk2=0;
+//float       furk3=0;                                             
+//float       furk4=0;
+//float       furk5=0;
+//
+//unsigned long previousMillis = 0;
+//const long interval = 200;
+//
+//void setup() {
+//  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+//  clock_prescale_set(clock_div_1);
+//#endif
+//
+//  strip.begin(); strip1.begin(); strip2.begin();           
+//  strip.show();  strip1.show();  strip2.show(); 
+//  strip.setBrightness(10); strip1.setBrightness(10); strip2.setBrightness(10); 
+//  Serial.begin (115200);
+//}
+//
+//void loop() {
+//  unsigned long currentMillis = millis();
+//  
+////nump es para el nivel fuerza visualizado en barra 61FCRcirc 1023FCRcuadra
+////nump numero de pixeles sensor de 0-(61 o 1023) y los pixeles van 0-8
+////furk fuerza en kilogramos de 0-(61 o 1023) y de 0 al limite superior
+////del sensor, para el cuadrado es 20N casi 2 Kg
+//
+//  golpe0= analogRead(pin);
+//  golpe1= analogRead(pin1);
+//  golpe2= analogRead(pin2);
+//  golpe3= analogRead(pin3);
+//  golpe4= analogRead(pin4);
+//  golpe5= analogRead(pin5);
+//
+//if (currentMillis - previousMillis >= interval) {
+//    previousMillis = currentMillis;
+//    Serial.print(golpe0); Serial.print(nump0);Serial.print('\t'); Serial.print(golpe1); Serial.print(nump1); Serial.print('\t');
+//    Serial.print(golpe2); Serial.print(nump2);Serial.print('\t'); Serial.print(golpe3); Serial.print(nump3);Serial.print('\t'); 
+//    Serial.print(golpe4); Serial.print(nump4);Serial.print('\t'); Serial.print(golpe5);Serial.print(nump5); Serial.println('\n');
+//
+//    Serial.print(furk0); Serial.print('\t'); Serial.print(furk1); Serial.print('\t'); Serial.print(furk2); 
+//    Serial.print('\t'); Serial.print(furk3); Serial.print('\t'); Serial.print(furk4); Serial.print('\t'); 
+//    Serial.print(furk5); Serial.println('\n');
+//}//delay(10);
+//
+//  if(golpe0>=600){
+//    nump0=map(golpe0, 600, 4095, 0, 8);                
+//    furk0=map(golpe0, 600, 4095, 0, 20);               
+//    }                                                  
+//
+//  if(golpe1>=600){
+//    nump1=map(golpe1, 600, 4095, 0, 8);                
+//    furk1=map(golpe1, 600, 4095, 0, 20);               
+//    }
+//
+//    if(golpe2>=600){
+//    nump2=map(golpe2, 600, 4095, 0, 8);                
+//    furk2=map(golpe2, 600, 4095, 0, 20);               
+//    }
+//
+//    if(golpe3>=600){
+//    nump3=map(golpe3, 600, 4095, 0, 8);                
+//    furk3=map(golpe3, 600, 4095, 0, 20);               
+//    }                                                  
+//
+//    if(golpe4>=600){
+//    nump4=map(golpe4, 600, 4095, 0, 8);                
+//    furk4=map(golpe4, 600, 4095, 0, 20);               
+//    }
+//
+//    if(golpe5>=600){
+//    nump5=map(golpe5, 600, 4095, 0, 8);                
+//    furk5=map(golpe5, 600, 4095, 0, 20);               
+//    }
+//  
+//  strip.clear();
+//  for(int i=0; (i<=(nump0 + nump3)); i++) {
+//    if (i<=1) rojo=0;
+//    if ((i>=2)&&(i<=4)) rojo=128;
+//    if (i>=4) rojo=255;
+//    if (i<=4) verde=255;
+//    if (i==5) verde=128;
+//    if (i>=6) verde=0;
+//    if (i<=8) azul=0;
+//    strip.setPixelColor(i, rojo, verde,  azul);//rojo,verde,azul 0-255
+//    strip.show();
+//    //delay(PAUSA);
+//  }
+//  strip1.clear();
+//  for(int j=0; (j<=(nump1 + nump4)); j++) {
+//    if (j<=1) rojo=0;
+//    if ((j>=2)&&(j<=4)) rojo=128;
+//    if (j>=4) rojo=255;
+//    if (j<=4) verde=255;
+//    if (j==5) verde=128;
+//    if (j>=6) verde=0;
+//    if (j<=8) azul=0;
+//    strip1.setPixelColor(j, rojo, verde,  azul);//rojo,verde,azul 0-255
+//    strip1.show();
+//    //delay(PAUSA);
+//  }
+//  strip2.clear();
+//  for(int j=0; (j<=(nump2 + nump5)); j++) {
+//    if (j<=1) rojo=0;
+//    if ((j>=2)&&(j<=4)) rojo=128;
+//    if (j>=4) rojo=255;
+//    if (j<=4) verde=255;
+//    if (j==5) verde=128;
+//    if (j>=6) verde=0;
+//    if (j<=8) azul=0;
+//    strip2.setPixelColor(j, rojo, verde,  azul);//rojo,verde,azul 0-255
+//    strip2.show();
+//    //delay(PAUSA);
+//  }
+//}//***fin loop
+
 
 //**********ACELERACION
 //------------------------------------------------------
-//#include <Wire.h>
-//#include <Adafruit_Sensor.h>
-//#include <Adafruit_ADXL345_U.h>
-//
-//Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345); //ID del sensor
-//float xac=0,yac=0,zac=0,x=0,y=0,z=0;
-//
-//void setup() 
-//{
-//  Serial.begin(9600);
-//  if(!accel.begin())
-//  {
-//    Serial.println("No se detecta sensor, verificar circuito");
-//    while(1);
-//  }
-//  accel.setRange(ADXL345_RANGE_16_G); //seleccion de rengo (8_G 4_G 2_G)
-//
-//  /* Eventos del sensor */ 
-//  sensors_event_t event; 
-//  accel.getEvent(&event);
-// 
-//  for(int i=0; i<10; ++i) {
-//    xac+=(event.acceleration.x); //el offset
-//    yac+=(event.acceleration.y);
-//    zac+=(event.acceleration.z);
-//    }
-//    xac/=10;Serial.print("xac ");Serial.print(xac);
-//    yac/=10;Serial.print("yac ");Serial.print(yac);
-//    zac/=10;Serial.print("zac ");Serial.println(zac);
-//  }
-//void loop() 
-//{
-//  /* Eventos del sensor */ 
-//  sensors_event_t event; 
-//  accel.getEvent(&event);    
-//  /* Aceleración en m/s^2) */
-//  x=abs((event.acceleration.x)-xac);
-//  y=abs((event.acceleration.y)-yac);
-//  z=abs((event.acceleration.z)-zac);
-//  Serial.print("X: "); Serial.print(x); Serial.print("  ");
-//  Serial.print("Y: "); Serial.print(y); Serial.print("  ");
-//  Serial.print("Z: "); Serial.print(z); Serial.print("  ");Serial.println("m/s^2 ");
-//  delay(10);
-//}
-//
-////mientras que el sensor sea diferente de cero hacer
-////
-////vx += ax * dt;
-////vy += ay * dt;
-////vz += az * dt;
-////v =raiz de vx2+vy2+vz2
 
-//------------------------------------------ACELERACION
+float X=2.335;  //replace your value 1.355 -*1.6505 -**2.335 
+float gx=0.776; //replace your value 0.776 -*0.2145 -**0.776
 
+float Y=1.84; //replace your value 1.889 -*1.7675 -**1.84
+float gy=0.173; //replace your value 0.033 -*0.0045 -**0.033
 
-//#include <Wire.h>
-//// Registers for ADXL345
-//#define ADXL345_ADDRESS (0xA6 >> 1)  // address for device is 8 bit but shift to the
-//                                     // right by 1 bit to make it 7 bit because the
-//                                     // wire library only takes in 7 bit addresses
-//#define ADXL345_REGISTER_XLSB (0x32)
-//
-//int accelerometer_data[3];
-//// void because this only tells the cip to send data to its output register
-//// writes data to the slave's buffer
-//void i2c_write(int address, byte reg, byte data) {
-//  // Send output register address
-//  Wire.beginTransmission(address);
-//  // Connect to device
-//  Wire.write(reg);
-//  // Send data
-//  Wire.write(data); //low byte
-//  Wire.endTransmission();
-//}
-//
-//// void because using pointers
-//// microcontroller reads data from the sensor's input register
-//void i2c_read(int address, byte reg, int count, byte* data) {
-//  // Used to read the number of data received
-//  int i = 0;
-//  // Send input register address
-//  Wire.beginTransmission(address);
-//  // Connect to device
-//  Wire.write(reg);
-//  Wire.endTransmission();
-//
-//  // Connect to device
-//  Wire.beginTransmission(address);
-//  // Request data from slave
-//  // Count stands for number of bytes to request
-//  Wire.requestFrom(address, count);
-//  while(Wire.available()) // slave may send less than requested
-//  {
-//    char c = Wire.read(); // receive a byte as character
-//    data[i] = c;
-//    i++;
-//  }
-//  Wire.endTransmission();
-//}
-//
-//void init_adxl345() {
-//  byte data = 0;
-//
-//  i2c_write(ADXL345_ADDRESS, 0x31, 0x0B);   // 13-bit mode  +_ 16g
-//  i2c_write(ADXL345_ADDRESS, 0x2D, 0x08);   // Power register
-//
-//
-//  i2c_write(ADXL345_ADDRESS, 0x1E, 0x00);   // x
-//  i2c_write(ADXL345_ADDRESS, 0x1F, 0x00);   // Y
-//  i2c_write(ADXL345_ADDRESS, 0x20, 0x05);   // Z
-// 
-//  // Check to see if it worked!
-//  i2c_read(ADXL345_ADDRESS, 0X00, 1, &data);
-//  if(data==0xE5)
-//    Serial.println("it work Success");
-//  else
-//    Serial.println("it work Fail");
-//}
-//
-//void read_adxl345() {
-//  byte bytes[6];
-//  memset(bytes,0,6);
-//
-//  // Read 6 bytes from the ADXL345
-//  i2c_read(ADXL345_ADDRESS, ADXL345_REGISTER_XLSB, 6, bytes);
-//  // Unpack data
-//  for (int i=0;i<3;++i) {
-//    accelerometer_data[i] = (int)bytes[2*i] + (((int)bytes[2*i + 1]) << 8);
-//  }
-//}
-//// initialise and start everything
-//void setup() {
-//  Wire.begin();
-//  Serial.begin(9600);
-//  for(int i=0; i<3; ++i) {
-//    accelerometer_data[i]  = 0;
-//  }
-//  init_adxl345();
-//}
-//
-//
-//void loop() {
-//  read_adxl345();
-//  Serial.print("ACCEL: ");
-//  Serial.print(float(accelerometer_data[0])*3.9/1000);//3.9mg/LSB scale factor in 13-bit mode
-//  Serial.print("\t");
-////  Serial.print(float(accelerometer_data[0]));
-////  Serial.print("\t");
-//  Serial.print(float(accelerometer_data[1])*3.9/1000);
-//  Serial.print("\t");
-//  Serial.print(float(accelerometer_data[2])*3.9/1000);
-//  Serial.print("\n");
-//  delay(200);
-//}
+float Z=1.57; //replace your value  1.738 -*1.736 -**1.57
+float gz=0.221; //replace your value  0.021 -*0.007 -**0.221
 
-
-#include <Adafruit_NeoPixel.h> //esp4095 19/05
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-#define NUMPIXELS   8
-#define led0        25  //GPIO25
-#define led1        26  //GPIO26 10  //IO10
-#define led2        27  //GPIO27
-Adafruit_NeoPixel strip(NUMPIXELS, led0, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip1(NUMPIXELS, led1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2(NUMPIXELS, led2, NEO_GRB + NEO_KHZ800);
-byte        rojo=0;
-byte        verde=0;
-byte        azul=0;
-const byte  PAUSA=500;
-
-const int pin = A0;
-const int pin1 = 39;
-const int pin2 = 34;
-const int pin3 = A3;
-const int pin4 = A4;
-const int pin5 = 13;//GPIO13
-
-int golpe0 = 0;  
-int golpe1 = 0;
-int golpe2 = 0;
-int golpe3 = 0;  
-int golpe4 = 0;
-int golpe5 = 0;
-
-byte        nump0=0;
-byte        nump1=0;
-byte        nump2=0;
-byte        nump3=0;
-byte        nump4=0;
-byte        nump5=0;
-
-float       furk0=0;                                             //Fuerza en Kilogramos byte porque es menor a 255
-float       furk1=0;
-float       furk2=0;
-float       furk3=0;                                             
-float       furk4=0;
-float       furk5=0;
-
-unsigned long previousMillis = 0;
-const long interval = 200;
-
-void setup() {
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-
-  strip.begin(); strip1.begin(); strip2.begin();           
-  strip.show();  strip1.show();  strip2.show(); 
-  strip.setBrightness(10); strip1.setBrightness(10); strip2.setBrightness(10); 
-  Serial.begin (115200);
+ 
+void setup()
+{
+  Serial.begin(9600);    //Baudrate 9600
 }
-
-void loop() {
-  unsigned long currentMillis = millis();
-  
-//nump es para el nivel fuerza visualizado en barra 61FCRcirc 1023FCRcuadra
-//nump numero de pixeles sensor de 0-(61 o 1023) y los pixeles van 0-8
-//furk fuerza en kilogramos de 0-(61 o 1023) y de 0 al limite superior
-//del sensor, para el cuadrado es 20N casi 2 Kg
-
-  golpe0= analogRead(pin);
-  golpe1= analogRead(pin1);
-  golpe2= analogRead(pin2);
-  golpe3= analogRead(pin3);
-  golpe4= analogRead(pin4);
-  golpe5= analogRead(pin5);
-
-if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    Serial.print(golpe0); Serial.print(nump0);Serial.print('\t'); Serial.print(golpe1); Serial.print(nump1); Serial.print('\t');
-    Serial.print(golpe2); Serial.print(nump2);Serial.print('\t'); Serial.print(golpe3); Serial.print(nump3);Serial.print('\t'); 
-    Serial.print(golpe4); Serial.print(nump4);Serial.print('\t'); Serial.print(golpe5);Serial.print(nump5); Serial.println('\n');
-
-    Serial.print(furk0); Serial.print('\t'); Serial.print(furk1); Serial.print('\t'); Serial.print(furk2); 
-    Serial.print('\t'); Serial.print(furk3); Serial.print('\t'); Serial.print(furk4); Serial.print('\t'); 
-    Serial.print(furk5); Serial.println('\n');
-}//delay(10);
-
-  if(golpe0>=600){
-    nump0=map(golpe0, 600, 4095, 0, 8);                
-    furk0=map(golpe0, 600, 4095, 0, 20);               
-    }                                                  
-
-  if(golpe1>=600){
-    nump1=map(golpe1, 600, 4095, 0, 8);                
-    furk1=map(golpe1, 600, 4095, 0, 20);               
-    }
-
-    if(golpe2>=600){
-    nump2=map(golpe2, 600, 4095, 0, 8);                
-    furk2=map(golpe2, 600, 4095, 0, 20);               
-    }
-
-    if(golpe3>=600){
-    nump3=map(golpe3, 600, 4095, 0, 8);                
-    furk3=map(golpe3, 600, 4095, 0, 20);               
-    }                                                  
-
-    if(golpe4>=600){
-    nump4=map(golpe4, 600, 4095, 0, 8);                
-    furk4=map(golpe4, 600, 4095, 0, 20);               
-    }
-
-    if(golpe5>=600){
-    nump5=map(golpe5, 600, 4095, 0, 8);                
-    furk5=map(golpe5, 600, 4095, 0, 20);               
-    }
-  
-  strip.clear();
-  for(int i=0; (i<=(nump0 + nump3)); i++) {
-    if (i<=1) rojo=0;
-    if ((i>=2)&&(i<=4)) rojo=128;
-    if (i>=4) rojo=255;
-    if (i<=4) verde=255;
-    if (i==5) verde=128;
-    if (i>=6) verde=0;
-    if (i<=8) azul=0;
-    strip.setPixelColor(i, rojo, verde,  azul);//rojo,verde,azul 0-255
-    strip.show();
-    //delay(PAUSA);
-  }
-  strip1.clear();
-  for(int j=0; (j<=(nump1 + nump4)); j++) {
-    if (j<=1) rojo=0;
-    if ((j>=2)&&(j<=4)) rojo=128;
-    if (j>=4) rojo=255;
-    if (j<=4) verde=255;
-    if (j==5) verde=128;
-    if (j>=6) verde=0;
-    if (j<=8) azul=0;
-    strip1.setPixelColor(j, rojo, verde,  azul);//rojo,verde,azul 0-255
-    strip1.show();
-    //delay(PAUSA);
-  }
-  strip2.clear();
-  for(int j=0; (j<=(nump2 + nump5)); j++) {
-    if (j<=1) rojo=0;
-    if ((j>=2)&&(j<=4)) rojo=128;
-    if (j>=4) rojo=255;
-    if (j<=4) verde=255;
-    if (j==5) verde=128;
-    if (j>=6) verde=0;
-    if (j<=8) azul=0;
-    strip2.setPixelColor(j, rojo, verde,  azul);//rojo,verde,azul 0-255
-    strip2.show();
-    //delay(PAUSA);
-  }
-}//***fin loop
+void loop()
+{
+  int x,y,z;
+  x=analogRead(0);
+  y=analogRead(1);
+  z=analogRead(2);
+  Serial.print("x= ");
+  Serial.print((x*5/1024.0 - X)/gx,2);
+  Serial.print(',');
+  Serial.print("y= ");
+  Serial.print(((y*5/1024.0- Y)/gy),2);
+  Serial.print(',');
+  Serial.print("z= ");
+  Serial.println(((z*5/1024.0-Z)/gz),2);
+  delay(100);
+}
